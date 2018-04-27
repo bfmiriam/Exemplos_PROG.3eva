8e2152e4302dd136418d31451da9e28b751d71c5
mbacelofernandez <mbacelofernandez@victoria12.danielcastelao.org>
2018-04-27 13:30:13 +0200

    exemplo GUI 2

---
+++ exemploGUI2/src/exemplogui2/ExemploGUI2.java
@@ -0,0 +1,13 @@
+
+package exemplogui2;
+
+
+public class ExemploGUI2 {
+
+  
+    public static void main(String[] args) {
+       Ventana obxVentana = new Ventana();
+       obxVentana.setVisible(true);
+    }
+    
+}
