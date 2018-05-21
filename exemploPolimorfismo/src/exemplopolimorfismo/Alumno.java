d3c2937d9dd8e6649f4c3f883473aa506edf8bca
mbacelofernandez <mbacelofernandez@victoria12.danielcastelao.org>
2018-05-21 09:03:44 +0200

    Exemplo polimorfismo

---
+++ exemploPolimorfismo/src/exemplopolimorfismo/Alumno.java
@@ -0,0 +1,38 @@
+
+package exemplopolimorfismo;
+
+
+public class Alumno extends Persoa {
+    
+    public int nota;
+
+    public Alumno() {
+    }
+
+    public Alumno(int nota) {
+        this.nota = nota;
+    }
+
+    public Alumno(int nota, String nome, int edade) {
+        super(nome, nota);
+        this.nota = nota;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        return "nome: "+super.getNome()+" edade: "+super.getEdade()+ " nota: " + nota;
+    }
+    
+    
+    
+    
+    
+}
---
+++ exemploPolimorfismo/src/exemplopolimorfismo/ExemploPolimorfismo.java
@@ -0,0 +1,17 @@
+
+package exemplopolimorfismo;
+
+
+public class ExemploPolimorfismo {
+
+
+    public static void main(String[] args) {
+      
+        Persoa a = new Alumno(6,"Ana",12);
+        System.out.println(a.toString());
+        
+        
+        
+    }
+    
+}
